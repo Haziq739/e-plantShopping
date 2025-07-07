dc5a2500e622c014bd2d34146bcf054d470d214c
Haziq739 <naeemhaziq345@gmail.com>
2025-07-07 23:16:28 +0500

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,6 @@ function App() {
         <div className="content">
          <div className="landing_content">
          <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
           </button>
